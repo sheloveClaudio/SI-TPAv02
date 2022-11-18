8c25fb48391082b9c2257baf47040a2c1e8ce440
sheloveClaudio <claudioramos2003@hotmail.com>
2022-11-18 17:04:00 +0000

    Version 0.3 - Reading values, Semi-Organized HTML

--- java/java.js
+++ java/java.js
@@ -1,8 +1,27 @@
-function convert_number(){
-    var numero = document.getElementById("inserted-number").value;
-    var conv_type = document.getElementById("number-type");
-    var conv_type = e.conv_type
-    if(conv_type=="decimal")
-        if(numero > 255 || numero < -128)
-            alert("O número tem mais que 8 bits!");
+function convert_number() {
+    const numero = document.getElementById("inserted-number").value;
+    var conv_type = document.getElementById("number-type").value;
+    
+    if(conv_type=="decimal"){
+        if (numero > 255 || numero < -128) {
+        document.getElementById("form").reset();
+        document.getElementById("conversion-msg").innerHTML = "<b>O número contém mais de 8 bits!</b>";
+        }else{
+            document.getElementById("recived-value").value = calcular(numero);
+            document.getElementById("conversion-msg").innerHTML = "<b>Conversão feita!</b>";
+        }
+        if (numero == 0) {
+            document.getElementById("form").reset();
+            document.getElementById("conversion-msg").innerHTML = "<b>Nenhum número inserido!</b>";
+            }
+    }
+}
+
+function calcular(a){
+    const num = a;
+    let x = a + 1;
+    return(x);
+}
+function limparp(){
+    document.getElementById("conversion-msg").innerHTML = "";
 }
