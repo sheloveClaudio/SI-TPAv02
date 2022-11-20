a03614cd75b3437af74570d5fdb114bbe798dc19
sheloveClaudio <claudioramos2003@hotmail.com>
2022-11-20 21:26:46 +0000

    Version 0.7 - Hexadecimal section added, Minor upgrades in number input logic

--- java/java.js
+++ java/java.js
@@ -1,15 +1,8 @@
-function convert_number() {
+    function convert_number() {
     var numero = parseInt(document.getElementById("inserted-number").value);
     var numero_convertido = document.getElementById("received-value");
     var conv_type = document.getElementById("number-type").value;
     var conv_type_result = document.getElementById("received-number-type").value;   
-
-
-// Caixa vazia
-if (numero == 0) {
-    document.getElementById("form").reset();
-    document.getElementById("conversion-msg").innerHTML = "<b>Nenhum número inserido!</b>";
-    }
 
 //    
 // Cima decimal
@@ -23,7 +16,9 @@ if (numero == 0) {
         if (numero > 255 || numero < -128) {
         document.getElementById("form").reset();
         document.getElementById("conversion-msg").innerHTML = "<b>O número contém mais de 8 bits!</b>";
-        }else{
+        }
+        
+        else{
             // Baixo decimal
             if(conv_type_result == "decimal"){
                 numero_convertido.value = numero;
@@ -45,6 +40,10 @@ if (numero == 0) {
                 document.getElementById("conversation-msg").innerHTML = "<b>Conversão feita!</b>"
             }      
         }
+        if (numero == 0 || document.getElementById("inserted-number").value == "") {
+            document.getElementById("form").reset();
+            document.getElementById("conversion-msg").innerHTML = "<b>Nenhum número inserido!</b>";
+            }
     }
 
 
@@ -88,6 +87,10 @@ if (numero == 0) {
                 document.getElementById("conversation-msg").innerHTML = "<b>Conversão feita!</b>"
             }
         }
+        if (numero == 0 || document.getElementById("inserted-number").value == "") {
+            document.getElementById("form").reset();
+            document.getElementById("conversion-msg").innerHTML = "<b>Nenhum número inserido!</b>";
+            }
     }
 
 
@@ -127,7 +130,54 @@ if (numero == 0) {
                 document.getElementById("conversation-msg").innerHTML = "<b>Conversão feita!</b>"
             }
         }
+        if (numero == 0 || document.getElementById("inserted-number").value == "") {
+            document.getElementById("form").reset();
+            document.getElementById("conversion-msg").innerHTML = "<b>Nenhum número inserido!</b>";
+            }
     }
+
+ 
+    
+
+//
+// Cima Hexadecimal
+//
+
+
+
+
+            else if(conv_type == "hexa"){
+                if(numero > 377){
+                    document.getElementById("form").reset();
+                    document.getElementById("conversion-msg").innerHTML = "<b>O número contém mais de 8 bits!</b>";
+                }
+                else{
+                    // Baixo decimal
+                    if(conv_type_result == "decimal"){
+                        numero_convertido.value = calculohexadecimaldec(document.getElementById("inserted-number").value);
+                        document.getElementById("conversion-msg").innerHTML = "<b>Conversão feita!</b>";
+                    }
+                    // Baixo binário
+                    else if(conv_type_result == "binario"){
+                        numero_convertido.value = calculohexadecimalbin(document.getElementById("inserted-number").value);
+                        document.getElementById("conversion-msg").innerHTML = "<b>Conversão feita!</b>";    
+                    }
+                    // Baixo octal
+                    else if(conv_type_result == "octal"){
+                        numero_convertido.value = calculohexadecimaloct(document.getElementById("inserted-number").value);
+                        document.getElementById("conversion-msg").innerHTML = "<b>Conversão feita!</b>";    
+                    }
+                    // Baixo hexadecimal
+                    else if(conv_type_result == "hexa"){
+                        numero_convertido.value = document.getElementById("inserted-number").value;
+                        document.getElementById("conversation-msg").innerHTML = "<b>Conversão feita!</b>"
+                    }
+                }
+                if (numero == 0 || document.getElementById("inserted-number").value == "") {
+                    document.getElementById("form").reset();
+                    document.getElementById("conversion-msg").innerHTML = "<b>Nenhum número inserido!</b>";
+                    }
+            }
 }
 
 // Verificação Binal
@@ -200,8 +250,37 @@ function calculooctalhex(a){
     return(fim);
 }
 
+// Calculo Hexa --> Outros
+
+function calculohexadecimaldec(a){
+    var fim = parseInt(a,16);
+    return(fim);
+}
+
+function calculohexadecimalbin(a){
+    var fim = parseInt(a,16).toString(2);
+    return(fim);
+}
+
+function calculohexadecimaloct(a){
+    var fim = parseInt(a,16).toString(8);
+
+}
+
 // Limpar texto 
 
 function limparp(){
     document.getElementById("conversion-msg").innerHTML = "";
 }
+
+function funcaodinamica(){
+    var conv_type = document.getElementById("number-type").value;
+
+    if(conv_type == "hexa"){
+        document.getElementById("inserted-number").type = "text";
+    }else document.getElementById("inserted-number").type = "number";
+}
+
+//if(conv_type == "hexa"){
+  //  document.getElementById('inserted-number').type = 'text';
+//}else document.getElementById('inserted-number').type = 'number';
